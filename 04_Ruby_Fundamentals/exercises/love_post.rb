bdaee1cdfd3095f7f8f4d884a3d1143ded7be6cf
Anne Garland Berry <annegarland.berry@gmail.com>
2015-08-11 17:41:20 -0700

    class notes 8.10

--- 04_Ruby_Fundamentals/exercises/love_post.rb
+++ 04_Ruby_Fundamentals/exercises/love_post.rb
@@ -2,12 +2,29 @@ require 'pry'
 require 'pry-byebug'
 
 def get_love_interest
+  puts "Who are you in love with?\n" #\n takes you to a new line
+  love_interest = gets.strip
+  capture_love_interest_response(love_interest) #if method reqires info, make sure that you are giving info
 end
 
-def capture_love_interest_response(love_interest)
+def capture_love_interest_response(love_interest) #ruby creates methods that describe what you're trying to do
+  puts "Is #{love_interest} on your mind?\n"
+  puts "Please give a 'yes' or 'no' answer \n"
+  user_answer = gets.strip.downcase
+  puts "your answer is #{user_answer}"
+  get_valid_answer(user_answer, love_interest)
 end
 
-def get_valid_answer(user_answer, love_interest)
+def get_valid_answer(user_answer, love_interest) #variables inside methods are available only inside that method
+case user_answer
+when "yes"
+  puts "Why don't you call #{love_interest}?"
+when "no"
+  puts "You should call #{love_interest}! You care about them!"
+else
+   puts "You moron. You did not put a valid answer"
+    puts capture_love_interest_response(love_interest)
+  end
 end
 
 ### This is where we call methods to run our script
